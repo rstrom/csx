dff5ed3596b91ff3a707b720bb595aa18a44ed36
Robbie Strom <robbie.strom@gmail.com>
2020-02-19 18:37:02 +0100

    Forward ref + better html attr typing

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,19 @@
 import { InterpolationWithTheme, jsx, SerializedStyles } from "@emotion/core";
-import React, { LegacyRef } from "react";
+import React from "react";
 import { keys } from "./util";
+
+type ElementFromTag<
+  T extends keyof React.ReactHTML
+> = React.ReactHTML[T] extends React.DetailedHTMLFactory<object, infer E>
+  ? E
+  : never;
+
+type AttributesFromElement<T> = T extends React.DetailedHTMLFactory<
+  infer U,
+  infer _
+>
+  ? U
+  : never;
 
 /**
  * Constructs a "styled but not styled-component" component with optional
@@ -10,28 +23,32 @@ import { keys } from "./util";
  * @param elementType html element type "button", "span", "svg" etc.
  */
 export const csx = <
-  E extends HTMLElement = HTMLDivElement,
-  S extends string = ""
+  S extends string = "",
+  T extends keyof React.ReactHTML = "div"
 >(
   baseStyles: SerializedStyles[],
-  conditionalStylesMap: Record<S, SerializedStyles> | {} = {},
-  elementType: keyof React.ReactDOM = "div"
-): React.FC<Partial<Record<S, boolean>> &
-  React.DetailedHTMLProps<React.HTMLAttributes<E>, E> & {
-    ref?: never;
-    innerRef?: LegacyRef<E | null>;
-    css?: InterpolationWithTheme<never>;
-  }> => props => {
-  const activeConditionals = keys(conditionalStylesMap)
-    .filter(k => !!props[k])
-    .map(k => conditionalStylesMap[k]);
-  // remove conditional styles so they are not passed as html attr
-  const nonStyleProps = keys(props)
-    .filter(k => !(k in conditionalStylesMap) && k !== "innerRef")
-    .reduce((p, k) => ({ ...p, [k]: props[k] }), {});
-  return jsx(elementType, {
-    ...nonStyleProps,
-    ref: props.innerRef,
-    css: [...baseStyles, ...activeConditionals, props.css]
+  conditionalStylesMap: Partial<Record<S, SerializedStyles>> = {},
+  elementType?: T
+) => {
+  return React.forwardRef<
+    ElementFromTag<T>,
+    Partial<{ [key in S]: boolean }> &
+      AttributesFromElement<React.ReactDOM[T]> &
+      React.PropsWithChildren<{
+        css?: InterpolationWithTheme<never>;
+      }>
+  >((props, ref) => {
+    const activeConditionals = keys(conditionalStylesMap)
+      .filter(k => !!props[k])
+      .map(k => conditionalStylesMap[k]);
+    // remove conditional styles so they are not passed as html attr
+    const nonStyleProps = keys(props)
+      .filter(k => !(k in conditionalStylesMap))
+      .reduce((p, k) => ({ ...p, [k]: props[k] }), {});
+    return jsx(elementType ?? "div", {
+      ...nonStyleProps,
+      ref: ref,
+      css: [...baseStyles, ...activeConditionals, props.css]
+    });
   });
 };
